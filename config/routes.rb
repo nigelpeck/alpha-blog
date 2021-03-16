5b1e3c2b329659a27d038753031b6ddc6b65095e
Nigel Peck <nigelpeck1966@gmail.com>
2021-03-16 16:12:09 +0000

    Added CRUD actions for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  get 'about', to: 'pages#about'
+  # this is how to expose the routes step by step if required
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  # this exposes all routes - including all REST-ful routes
+  # REST == Representation state transfer - mappung HTTP vers (get, post, put / patch, delete) to CRUD actions
+  resources :articles
 end
